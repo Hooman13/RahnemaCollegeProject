37f44983230f033ea1fbc0e7eb746dd218c152d9
Hooman <hooman.torabi18@gmail.com>
2024-08-07 01:16:23 +0330

    use zod to add validations to fields

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -2,11 +2,41 @@ import { Link } from "react-router-dom";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const FormSchema = z.object({
+  username: z
+    .string()
+    .min(3, "Username must not be lesser than 3 characters")
+    .regex(
+      /^[a-zA-Z0-9_]+$/,
+      "The username must contain only letters, numbers and underscore (_)"
+    ),
+  email: z.string().email("Invalid email. Email must be a valid email address"),
+  password: z.string().min(8, "Password must not be lesser than 8 characters"),
+  confirmPassword: z
+    .string()
+    .min(8, "Password must not be lesser than 8 characters"),
+});
+
+type IFormInput = z.infer<typeof FormSchema>;
 
 export const Signup = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>({
+    resolver: zodResolver(FormSchema),
+  });
 
-  const onSubmit = (data: {}) => console.log(data);
+  const onSubmit = (data: IFormInput) => {
+    console.log(data);
+  };
+
+  // const { register, handleSubmit } = useForm();
+
+  // const onSubmit = (data: {}) => console.log(data);
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
@@ -38,24 +68,36 @@ export const Signup = () => {
                   placeholder="نام کاربری"
                   className="border rounded-2xl w-full text-right text-base mb-6 px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600"
                 />
+                {errors?.username?.message && (
+                  <p className="text-red-700 mb-4">{errors.username.message}</p>
+                )}
                 <input
                   type="text"
                   {...register("email", { required: true })}
                   placeholder="ایمیل"
                   className="border text-right rounded-2xl w-full text-base mb-6 px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600"
                 />
+                {errors?.email?.message && (
+                  <p className="text-red-700 mb-4">{errors.email.message}</p>
+                )}
                 <input
                   type="text"
                   {...register("password")}
                   placeholder="رمز عبور"
                   className="border text-right rounded-2xl w-full text-base mb-6 px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600"
                 />
+                {errors?.password?.message && (
+                  <p className="text-red-700 mb-4">{errors.password.message}</p>
+                )}
                 <input
                   type="text"
                   {...register("confirmPassword")}
                   placeholder="تکرار رمز عبور"
                   className="border text-right rounded-2xl w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600"
                 />
+                {errors?.password?.message && (
+                  <p className="text-red-700 mb-4">{errors.password.message}</p>
+                )}
               </div>
               <div className="text-center mt-6 flex border-solid rounded-2xl bg-[#EA5A69] w-[84px] ml-auto mr-auto justify-center items-center px-[16px] py-[8px] ">
                 <button type={"submit"}>ثبت نام</button>
