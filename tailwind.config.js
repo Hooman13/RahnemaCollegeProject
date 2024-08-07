928c374c748c3934739b0551b231aa0186d23d01
Hooman <hooman.torabi18@gmail.com>
2024-08-07 04:07:36 +0330

    install font awesome

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+const { faVectorSquare } = require("@fortawesome/free-solid-svg-icons");
+const { url } = require("inspector");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
