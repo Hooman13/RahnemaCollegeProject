1ebf506129a1c09513a7cb6bf7446101bc083ee4
mostafa hosseini <mostafa.hosseini@digikala.com>
2024-08-09 14:35:13 +0330

    feat:using RequireAuth comp for loginBasePage routes

--- src/pages/LoginBasePage.tsx
+++ src/pages/LoginBasePage.tsx
@@ -5,18 +5,25 @@ import { PassRecovery } from "./PassRecovery";
 import { Signup } from "./Signup";
 import { Profile } from "./Profile";
 import { EmailSent } from "./EmailSent";
+import RequireAuth from "../components/RequireAuth";
 
 export const LoginBasePage = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          {/* Public Routes */}
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/forgotpass" element={<PassRecovery />} />
           <Route path="/reset-password" element={<NewPass />} />
-          <Route path="/profile" element={<Profile />} />
           <Route path="/email-sent-page" element={<EmailSent />} />
+
+          {/* Protected Routes */}
+          <Route element={<RequireAuth />}>
+            <Route path="/" element={<Profile />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
