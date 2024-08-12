a9cec55705dbda24c8bf166149eaefb1bc1378fb
Hooman <hooman.torabi18@gmail.com>
2024-08-12 16:43:45 +0330

    last changes in mypage and profile page

--- src/components/MyPage.tsx
+++ src/components/MyPage.tsx
@@ -2,25 +2,55 @@ import styles from "./MyPage.module.css";
 import { Link } from "react-router-dom";
 export const MyPage = () => {
   return (
-    <div className={`${styles.myPage}`}>
-      <h1 className={`${styles.pageTitle}`}>صفحه من</h1>
-
-      <div className={`${styles.userAvatarBox}`}>
-        <img src="./img/avatar.png" className="user-avatar" alt="" />
+    <>
+      <div>
+        {/* profile informations */}
+        <div className="text-black border-b-2 border-[#CAC4D0] pb-6">
+          <h1 className="mb-8 text-xl">صفحه من</h1>
+          <div className="flex justify-between">
+            <div className="flex items-center">
+              <img
+                className="border rounded-full ml-8 w-[132px] h-[132px]"
+                src="./img/avatar.png"
+                alt=""
+              />
+              <div className="grid grid-rows-4 h-[132px]">
+                <div className="user-display-name text-sm text-[#C19008]">
+                  @mahmz
+                </div>
+                <div className="user-full-name text-xl">مهشید منزه</div>
+                <div className="text-sm flex  ">
+                  <div className="user-followers-details pl-2 text-[#C19008]">
+                    13 دنبال کننده
+                  </div>
+                  |
+                  <div className="user-followers-details px-2 text-[#C19008]">
+                    13 دنبال شونده
+                  </div>
+                  |<div className="user-followers-details pr-2">13 پست</div>
+                </div>
+                <div className="user-followers-details w-[377px]">
+                  Lover, not a fighter, spreading ✌️all over the 🌎
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center justify-items-end	">
+              <Link to="/editpage">
+                <button
+                  type="button"
+                  className="w-[197px] py-4 px-2 bg-[#EA5A69] rounded-3xl text-white "
+                >
+                  ویرایش پروفایل
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        {/* posts place */}
+        <div className="mt-6 bg-black h-full border rounded-t-[45px]">
+          hello
+        </div>
       </div>
-
-      <div className={`${styles.userDetails}`}>
-        <p className="user-display-name">mahnaz</p>
-        <p className="user-full-name">مهشید منزه</p>
-        <p className="user-followers-details">13 دنبال کننده</p>
-      </div>
-      <Link to="/editpage">
-        <button type="button" className={`${styles.editProfileBtn}`}>
-          ویرایش پروفایل
-        </button>
-      </Link>
-
-      <hr />
-    </div>
+    </>
   );
 };
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -16,84 +16,86 @@ import { MyPage } from "../components/MyPage";
 
 export const Profile = () => {
   return (
-    <div className="w-screen h-screen pt-16 px-16 bg-[#F5F5F5]">
-      {/* header */}
-      <div className="grid grid-cols-12 mb-4">
-        <div className="grid col-span-3 py-4 justify-items-center	">
-          <button
-            type="button"
-            className="w-[232px] py-2 bg-[#EA5A69] rounded-3xl text-white"
-          >
-            <FontAwesomeIcon className="ml-2" icon={faCirclePlus} />
-            ایجاد پست جدید
-          </button>
+    <>
+      <div className="w-screen h-full pt-16 px-16 bg-[#F5F5F5]">
+        {/* header */}
+        <div className="grid grid-cols-12 mb-4">
+          <div className="grid col-span-3 py-4 justify-items-center	">
+            <button
+              type="button"
+              className="w-[232px] py-4 px-2 bg-[#EA5A69] rounded-3xl text-white"
+            >
+              <FontAwesomeIcon className="ml-2" icon={faCirclePlus} />
+              ایجاد پست جدید
+            </button>
+          </div>
+          <div className=" col-span-9 flex justify-end">
+            <img src="./img/logo.png" alt="" />
+          </div>
         </div>
-        <div className=" col-span-9 flex justify-end">
-          <img src="./img/logo.png" alt="" />
+        {/* main */}
+        <div className="grid grid-cols-12">
+          {/* sideBar */}
+          <div className="h-full w-auto col-span-3 bg-white border border-white rounded-t-[45px] justify-items-center	">
+            <div>
+              <div className="w-[296px] items-center flex h-20 m-6 py-4 px-8">
+                <img
+                  className="border rounded-full w-12 h-12"
+                  src="./img/avatar.png"
+                  alt=""
+                />
+                <span className="px-4 py-3">mahnaz</span>
+              </div>
+              <div className="w-[296px] items-center py-4 flex h-14 pr-9 px-8 text-center">
+                <Link to="/editpage">
+                  <FontAwesomeIcon className="ml-4" icon={faThumbTack} />
+                  صفحه من
+                </Link>
+              </div>
+              <div className="w-[296px] items-center py-4 flex h-14 pr-9 px-8 text-center">
+                <Link to="/editpage">
+                  <FontAwesomeIcon className="ml-4" icon={faBookmark} />
+                  ذخیره‌ها
+                </Link>
+              </div>
+              <div className="w-[296px] items-center py-4 flex h-14 pr-9 px-8 text-center">
+                <Link to="/editpage">
+                  <FontAwesomeIcon className="ml-4" icon={faCommentDots} />
+                  پیام‌ها
+                </Link>
+              </div>
+              <div className="w-[296px] items-center py-4 flex h-14 pr-9 px-8 text-center">
+                <Link to="/editpage">
+                  <FontAwesomeIcon className="ml-4" icon={faBell} />
+                  اعلانات
+                </Link>
+              </div>
+              <div className="w-[296px] items-center py-4 flex h-14 pr-9 px-8 text-center">
+                <Link to="/editpage">
+                  <FontAwesomeIcon className="ml-4" icon={faTag} />
+                  تگ‌شده‌ها
+                </Link>
+              </div>
+            </div>
+            <div className="border-t-2 m-6"></div>
+            <div className="w-[296px] items-center py-4 flex h-14 pr-9 px-8 text-center">
+              <Link to="/editpage">
+                <FontAwesomeIcon className="ml-4" icon={faGripVertical} />
+                اکسپلور
+              </Link>
+            </div>
+            <div className="w-[296px] items-center py-4 flex h-14 pr-9 px-8 text-center">
+              <Link to="/editpage">
+                <FontAwesomeIcon className="ml-4" icon={faMagnifyingGlass} />
+                جستجو
+              </Link>
+            </div>
+          </div>
+          <div className="mr-16   grid col-span-9">
+            <MyPage />
+          </div>
         </div>
       </div>
-      {/* main */}
-      <div className="grid grid-cols-12">
-        {/* sideBar */}
-        <div className="h-screen w-auto col-span-3 bg-white border border-white rounded-t-[45px] justify-items-center	">
-          <div>
-            <div className="w-[296px] items-center flex h-20 m-6 py-4 px-8">
-              <img
-                className="border rounded-full w-12 h-12"
-                src="./img/avatar.png"
-                alt=""
-              />
-              <span className="px-4 py-3">mahnaz</span>
-            </div>
-            <div className="w-[296px] items-center py-4 flex h-14 pr-9 px-8 text-center">
-              <Link to="/editpage">
-                <FontAwesomeIcon className="ml-4" icon={faThumbTack} />
-                صفحه من
-              </Link>
-            </div>
-            <div className="w-[296px] items-center py-4 flex h-14 pr-9 px-8 text-center">
-              <Link to="/editpage">
-                <FontAwesomeIcon className="ml-4" icon={faBookmark} />
-                ذخیره‌ها
-              </Link>
-            </div>
-            <div className="w-[296px] items-center py-4 flex h-14 pr-9 px-8 text-center">
-              <Link to="/editpage">
-                <FontAwesomeIcon className="ml-4" icon={faCommentDots} />
-                پیام‌ها
-              </Link>
-            </div>
-            <div className="w-[296px] items-center py-4 flex h-14 pr-9 px-8 text-center">
-              <Link to="/editpage">
-                <FontAwesomeIcon className="ml-4" icon={faBell} />
-                اعلانات
-              </Link>
-            </div>
-            <div className="w-[296px] items-center py-4 flex h-14 pr-9 px-8 text-center">
-              <Link to="/editpage">
-                <FontAwesomeIcon className="ml-4" icon={faTag} />
-                تگ‌شده‌ها
-              </Link>
-            </div>
-          </div>
-          <div className="border-t-2 m-6"></div>
-          <div className="w-[296px] items-center py-4 flex h-14 pr-9 px-8 text-center">
-            <Link to="/editpage">
-              <FontAwesomeIcon className="ml-4" icon={faGripVertical} />
-              اکسپلور
-            </Link>
-          </div>
-          <div className="w-[296px] items-center py-4 flex h-14 pr-9 px-8 text-center">
-            <Link to="/editpage">
-              <FontAwesomeIcon className="ml-4" icon={faMagnifyingGlass} />
-              جستجو
-            </Link>
-          </div>
-        </div>
-        <div className="bg-black  mr-16 grid col-span-9">
-          <MyPage />
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
