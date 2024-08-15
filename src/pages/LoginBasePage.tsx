5e5a156a4724ec309f1acb790674460dc618b3a4
mostafa hosseini <smostafah89@gmail.com>
2024-08-16 03:02:24 +0330

    feat:add saved and profile page to protected routes

--- src/pages/LoginBasePage.tsx
+++ src/pages/LoginBasePage.tsx
@@ -9,6 +9,7 @@ import { EditProfile } from "./EditProfile";
 import RequireAuth from "../components/RequireAuth";
 import { ErrorPage } from "./Error";
 import { AddPhoto } from "../components/AddPhoto";
+import { Saved } from "./Saved";
 
 export const LoginBasePage = () => {
   return (
@@ -21,13 +22,14 @@ export const LoginBasePage = () => {
           <Route path="/forgotpass" element={<PassRecovery />} />
           <Route path="/reset-password" element={<NewPass />} />
           <Route path="/email-sent-page" element={<EmailSent />} />
-          {/* <Route path="/profile" element={<Profile />} /> */}
           <Route path="/error" element={<ErrorPage />} />
           <Route path="/addphoto" element={<AddPhoto />} />
 
           {/* Protected Routes */}
           <Route element={<RequireAuth />}>
             <Route path="/" element={<Profile />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/saved" element={<Saved />} />
             <Route path="/editpage" element={<EditProfile />} />
           </Route>
         </Routes>
