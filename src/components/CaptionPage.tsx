ef42e2154b43f3424f31bdff8a9d7dba0c0cb675
Hooman <hooman.torabi18@gmail.com>
2024-08-16 11:30:17 +0330

    log the caption data

---
+++ src/components/CaptionPage.tsx
@@ -0,0 +1,143 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import styles from "./Profile.module.css";
+import { useNavigate } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faThumbTack,
+  faBookmark,
+  faCommentDots,
+  faBell,
+  faTag,
+  faMagnifyingGlass,
+  faGripVertical,
+  faCirclePlus,
+} from "@fortawesome/free-solid-svg-icons";
+import { MyPage } from "../components/MyPage";
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+import { boolean, string } from "zod";
+
+const FormSchema = z.object({
+  caption: z.string().optional(),
+});
+
+type IFormInput = z.infer<typeof FormSchema>;
+
+export const CaptionPage = () => {
+  const token = Cookies.get("token");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const navigate = useNavigate();
+  const handleProfileEdited = () => {
+    navigate("");
+  };
+
+  const onSubmit = (data: IFormInput) => {
+    console.log(data);
+    // axios
+    //   .post("http://37.32.5.72:3000/", JSON.stringify(data), {
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       Authorization: `Bearer ${token}`,
+    //     },
+    //   })
+    //   .then((response) => {
+    //     // if (response.status === 200) {
+    //     //   handleProfileEdited();
+    //     // }
+    //   })
+    //   .catch((err) => console.log(err));
+  };
+
+  const [formInput, setFormInput] = useState({
+    caption: "",
+  });
+
+  const handleUserInput = (name: string, value?: string) => {
+    setFormInput({
+      ...formInput,
+      [name]: value,
+    });
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <section>
+          <div
+            className="frame5 w-screen h-screen bg-no-repeat bg-center bg-cover flex justify-center items-center"
+            style={{ backgroundImage: "url(./img/login-background.png)" }}
+          >
+            <div className="bg-white w-screen md:w-[485px] h-screen md:h-auto  py-16 shadow-lg rounded-3xl mt-3 px-20 ">
+              {/* The Graph */}
+              <div className="flex-row flex justify-evenly px-4 py-6 ">
+                <div className="grid grid-rows-2 justify-items-center">
+                  <div className=" border justify-items-center justify-center items-center  border-[#6F6F6F] rounded-full w-4 h-4">
+                    {/* <div className="flex items-center justify-center border m-auto   border-black rounded-full w-1 h-1 bg-black"></div> */}
+                  </div>
+                  <p className="grid text-[#6F6F6F] row-span-1 text-[10px] mt-1">
+                    تنظیمات
+                  </p>
+                </div>
+                <div className="grid grid-rows-2 justify-items-center">
+                  <div className="flex border justify-items-center justify-center items-center  border-black rounded-full w-4 h-4">
+                    <div className="flex items-center justify-center border m-auto   border-black rounded-full w-1 h-1 bg-black"></div>
+                  </div>
+                  <p className="grid row-span-1 text-[10px] mt-1">متن</p>
+                </div>
+                <div className="grid grid-rows-2 justify-items-center">
+                  <div className=" border justify-items-center justify-center items-center   border-[#6F6F6F] rounded-full w-4 h-4">
+                    {/* <div className="flex items-center justify-center border m-auto   border-black rounded-full w-1 h-1 bg-black"></div> */}
+                  </div>
+                  <p className="grid text-[#6F6F6F] row-span-1 text-[10px] mt-1">
+                    عکس
+                  </p>
+                </div>
+              </div>
+              {/* main */}
+              <div className="text-center mt-8 text-base font-normal mb-8">
+                <p>کپشن مورد نظرت رو بنویس:</p>
+              </div>
+              <div className="flex justify-center mb-8">
+                <div className="text-right text-sm font-bold mb-6 ">
+                  <p className="text-[#191919] pb-2">کپشن</p>
+                  <input
+                    className="w-[320px] h-[88px] border solid border-[#17494D]/50 rounded-xl"
+                    type="text"
+                    {...register("caption")}
+                    value={formInput.caption}
+                    onChange={({ target }) => {
+                      handleUserInput(target.name, target.value);
+                    }}
+                  />
+                </div>
+              </div>
+              {/* buttons */}
+              <div className="flex items-center justify-end text-sm">
+                <div className="flex pl-5">
+                  <Link to="/">
+                    <button>پشیمون شدم</button>
+                  </Link>
+                </div>
+                <div className="text-center mr-1 flex border-solid rounded-2xl bg-[#EA5A69] w-[62px] h-[36px] text-sm justify-center items-center px-[8px] py-[16px] ">
+                  <button type={"submit"}>بعدی</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </form>
+    </>
+  );
+};
