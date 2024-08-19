b6725d295564ad82ba77f1346b1d82ca79242eb5
Hooman <hooman.torabi18@gmail.com>
2024-08-19 23:18:59 +0330

    create followercard component

---
+++ src/components/FollowerCard.tsx
@@ -0,0 +1,24 @@
+export const FollowerCard = () => {
+  return (
+    <>
+      <div className="grid grid-cols-6 justify-between items-center  h-14  text-xl text-center">
+        <div className="items-center col-span-4 flex justify-start w-[210px] h-14">
+          <img
+            className="border rounded-full ml-7 w-[56px] h-[56px]"
+            src="./img/avatar.png"
+            alt=""
+          />
+          <div className="grid grid-rows-2 text-right">
+            <div className="user-display-name text-sm h-6 font-bold">
+              متین دهقان
+            </div>
+            <div className="user-full-name text-xs h-6 font-normal ">
+              170 هزار دنبال‌کننده
+            </div>
+          </div>
+        </div>
+        <div className="col-span-2 mr-4 items-end">. . .</div>
+      </div>
+    </>
+  );
+};
--- src/pages/FollowersList.tsx
+++ src/pages/FollowersList.tsx
@@ -1,3 +1,5 @@
+import { FollowerCard } from "../components/FollowerCard";
+
 export const FollowersList = () => {
   return (
     <>
@@ -7,14 +9,11 @@ export const FollowersList = () => {
             className="frame5 w-screen h-screen bg-no-repeat bg-center bg-cover flex justify-center items-center"
             style={{ backgroundImage: "url(./img/login-background.png)" }}
           >
-            <div className=" bg-white w-screen md:w-[485px] h-screen md:h-auto  py-16 shadow-lg rounded-3xl mt-3 px-20 ">
-              <div className="flex justify-center pb-10">
-                <img src="./img/logo.png" alt="" />
+            <div className=" bg-white w-screen md:w-[500px] h-screen md:h-auto  py-16 shadow-lg rounded-3xl mt-3 px-12 ">
+              <div className="flex justify-center pb-10 text-xl font-bold text-[#191919]">
+                دنبال شونده‌‌ها
               </div>
-              <div className="text-xl text-center">
-                لینک تغییر رمز عبور ارسال شد. برای تنظیم رمز جدید لطفاً
-                ایمیل‌تون رو چک کنید.
-              </div>
+              <FollowerCard />
             </div>
           </div>
         </form>
