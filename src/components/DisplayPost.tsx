1a912186c09b2b23f8d17231d3d31f0568b8bcaa
mostafa hosseini <smostafah89@gmail.com>
2024-08-22 23:28:01 +0330

    feat:add post endpoint api to get post details

--- src/components/DisplayPost.tsx
+++ src/components/DisplayPost.tsx
@@ -1,104 +1,114 @@
-import { faBookmark, faHeart,faComment } from "@fortawesome/free-regular-svg-icons";
+import {
+  faBookmark,
+  faHeart,
+  faComment,
+} from "@fortawesome/free-regular-svg-icons";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Axios from "axios";
 import { Carousel, Badge } from "flowbite-react";
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { ToastR } from "./controles/ToastR";
 
 export const DisplayPost: React.FC = () => {
   const { postId } = useParams();
+
+  interface IImage {
+    imageId: string;
+    url: string;
+  }
+  interface ICreator {
+    username: string;
+    imageUrl: string;
+  }
+  interface Ipost {
+    postId: string;
+    caption: string;
+    creator: ICreator;
+    createdAt: string;
+    updatedAt: string;
+    tags: string[];
+    imageInfos: IImage[];
+    likeCount: number;
+    commentsCount: number;
+    bookMarkCount: number;
+  }
+
+  const [post, setpost] = useState<Ipost>({
+    postId: "",
+    caption: "",
+    creator: {
+      username: "",
+      imageUrl: "",
+    },
+    createdAt: "",
+    updatedAt: "",
+    tags: [],
+    imageInfos: [],
+    likeCount: 0,
+    commentsCount: 0,
+    bookMarkCount: 0,
+  });
+
   const getPostDetails = () => {
     try {
       Axios.get(`http://37.32.5.72:3000/posts/${postId}`).then((res) => {
-        debugger;
+        const postData = res.data;
+        setpost((prev) => ({
+          ...prev,
+          ...postData,
+        }))
       });
     } catch (error) {
       console.log(error);
+      <ToastR type="danger"></ToastR>
     }
   };
 
-  const post = {
-    postId: "f8a9183-ce9c-4c6e-a728-e948167befca",
-    creator: {
-      username: "mostafa",
-      imageUrl: "./img/avatar.png",
-    },
-    imageInfos: [
-      {
-        url: "./img/post-images/n1.jpg",
-        imageId: "f8a9183-ce9c-4c6e-a728-e948167befca",
-      },
-      {
-        url: "./img/post-images/n2.jpg",
-        imageId: "f8a9183-ce9c-4c6e-a728-e948167befca",
-      },
-      {
-        url: "./img/post-images/n3.jpg",
-        imageId: "f8a9183-ce9c-4c6e-a728-e948167befca",
-      },
-    ],
-    caption: "یک روز در جنگل",
-    tags: ["جنگل", "طبیعت", "درخت"],
-    commentsCount: "12",
-    likeCount: "233",
-    bookMarkCount: "12",
-  };
+  useEffect(() => {
+    getPostDetails();
+  }, []);
 
-  //   useEffect(() => {
-  //     getPostDetails();
-  //   }, []);
   return (
     <>
       <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
         <header className="mb-4 lg:mb-6 not-format">
           <address className="flex items-center mb-6 not-italic">
             <div className="grow text-sm text-gray-900 dark:text-white">
-              <a
-                href="#"
+              <Link
+                to={"/profile/" + post.creator.username}
                 rel="author"
                 className="flex text-base items-center font-bold text-gray-900 dark:text-white"
               >
                 <img
                   className="w-16 h-16 rounded-full ml-3"
-                  src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
-                  alt="Mostafa"
+                  src={post.creator.imageUrl}
+                  alt={post.creator.username}
                 />
-                Mostafa
-              </a>
+                {post.creator.username}
+              </Link>
             </div>
             <button
               type="button"
               className="w-auto py-4 px-2 bg-[#EA5A69] rounded-3xl text-white "
             >
+              <FontAwesomeIcon icon={faPen} />
               ویرایش پست
             </button>
           </address>
         </header>
 
-        <div className="mb-14 h-56 sm:h-64 xl:h-80 2xl:h-96">
-          <Carousel className="rounded-t-lg" dir="ltr">
-            <img
-              src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
-              alt="..."
-            />
-            <img
-              src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
-              alt="..."
-            />
-            <img
-              src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
-              alt="..."
-            />
-            <img
-              src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
-              alt="..."
-            />
-            <img
-              src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
-              alt="..."
-            />
-          </Carousel>
-        </div>
+        {post.imageInfos.length && (
+          <div className="mb-14 h-56 sm:h-64 xl:h-80 2xl:h-96">
+            <Carousel className="rounded-t-lg" dir="ltr">
+              {post.imageInfos.map((item, index) => {
+                return <img key={index} src={"http://37.32.5.72/"+item.url} alt="image" />;
+              })}
+            </Carousel>
+          </div>
+        )}
+
         <p
           className="text-xs font-normal
          text-neutral-800 dark:text-gray-400"
@@ -106,37 +116,30 @@ export const DisplayPost: React.FC = () => {
           <time title="February 8th, 2022">دو ماه پیش</time>
         </p>
         <p className="text-sm font-normal text-neutral-800 my-4">
-          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
-          استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
-          ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و
-          کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی
-          در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می
-          طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی
-          الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این
-          صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و
-          شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای
-          اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده
-          قرار گیرد.
+          {post.caption}
         </p>
         <div className="flex flex-wrap gap-2">
-          <Badge size="sm" href="#" color="info">بومگردی</Badge>
-          <Badge size="sm" href="#" color="info">بومگردی</Badge>
-          <Badge size="sm" href="#" color="info">بومگردی</Badge>
-          <Badge size="sm" href="#" color="info">بومگردی</Badge>
+          {post.tags.map((item, index) => {
+            return (
+              <Badge key={index} size="sm" href="#" color="info">
+                {item}
+              </Badge>
+            );
+          })}
         </div>
 
         <div className="flex flex-row-reverse items-center h-14 pt-4 mb-8 font-medium text-[#EA5A69]">
           <div className="flex-none w-9 gap-2 ">
-            <FontAwesomeIcon icon={faBookmark}/>
-            <div>15</div>
+            <FontAwesomeIcon icon={faBookmark} />
+            <div>{post.bookMarkCount}</div>
           </div>
           <div className="flex-none w-9 gap-2">
-            <FontAwesomeIcon icon={faHeart}/>
-            <div>15</div>
+            <FontAwesomeIcon icon={faHeart} />
+            <div>{post.likeCount}</div>
           </div>
           <div className="flex-none w-9 gap-2">
-            <FontAwesomeIcon icon={faComment}/>
-            <div>15</div>
+            <FontAwesomeIcon icon={faComment} />
+            <div>{post.commentsCount}</div>
           </div>
         </div>
       </article>
