cb63a5e0983563eef245a13136f340ca8e9a57b5
mostafa hosseini <smostafah89@gmail.com>
2024-08-23 15:41:19 +0330

    fix:correct properties for items

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -2,13 +2,11 @@ import { FunctionComponent, PropsWithChildren } from "react";
 import { Link } from "react-router-dom";
 interface IPostProps {
   id: string;
-  caption: string;
-  imgUrl: string; //first image only
+  imgUrl: string;
 }
 export const PostItem: FunctionComponent<PropsWithChildren<IPostProps>> = ({
   children,
   id,
-  caption,
   imgUrl,
 }) => {
 
@@ -17,15 +15,12 @@ export const PostItem: FunctionComponent<PropsWithChildren<IPostProps>> = ({
     <figure id={id} className="relative max-w-sm cursor-pointer hover:shadow-lg">
       <Link to={postUrl}>
         <img
-          className="h-auto max-w-full  rounded-t-lg aspect-square object-cover transition-transform duration-300 transform hover:scale-105 peer"
+          className="h-48 max-w-full  rounded-t-lg aspect-square object-cover transition-transform duration-300 transform hover:scale-105 peer"
           src={imgUrl}
-          alt={caption}
+          alt=""
         />
       </Link>
       <figcaption className="absolute px-4 text-sm text-white bottom-6 w-100">
-        <p>
-         {caption}
-        </p>
       </figcaption>
     </figure>
   );
