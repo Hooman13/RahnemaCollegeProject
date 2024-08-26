ff7cb3e09a1de75d638c7ac61713208ef5bb073e
Hooman <hooman.torabi18@gmail.com>
2024-08-26 10:32:58 +0330

    change hover color of sidebar icons

--- src/components/ProfileSidebar.tsx
+++ src/components/ProfileSidebar.tsx
@@ -59,31 +59,31 @@ export const ProfileSidebar: FunctionComponent = () => {
           />
           <span className="px-4 py-3">{user.data.username}</span>
         </div>
-        <div className="w-auto text-base 2xl:text-xl font-normal items-center py-4 flex h-auto pr-9 px-8 hover:bg-[#F2F2F7] border-none rounded-[75px] text-center">
+        <div className="w-auto text-base 2xl:text-xl font-normal items-center py-4 flex h-auto pr-9 px-8 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
           <Link to="/profile">
             <FontAwesomeIcon className="ml-4" icon={faThumbTack} />
             صفحه من
           </Link>
         </div>
-        <div className="w-auto text-base 2xl:text-xl font-normal items-center py-4 flex h-auto pr-9 px-8 hover:bg-[#F2F2F7] border-none rounded-[75px] text-center">
+        <div className="w-auto text-base 2xl:text-xl font-normal items-center py-4 flex h-auto pr-9 px-8 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
           <Link to="/saved">
             <FontAwesomeIcon className="ml-4" icon={faBookmark} />
             ذخیره‌ها
           </Link>
         </div>
-        <div className="w-auto text-base 2xl:text-xl font-normal items-center py-4 flex h-auto pr-9 px-8 hover:bg-[#F2F2F7] border-none rounded-[75px] text-center">
+        <div className="w-auto text-base 2xl:text-xl font-normal items-center py-4 flex h-auto pr-9 px-8 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
           <Link to="/messages">
             <FontAwesomeIcon className="ml-4" icon={faCommentDots} />
             پیام‌ها
           </Link>
         </div>
-        <div className="w-auto text-base 2xl:text-xl font-normal items-center py-4 flex h-auto pr-9 px-8 hover:bg-[#F2F2F7] border-none rounded-[75px] text-center">
+        <div className="w-auto text-base 2xl:text-xl font-normal items-center py-4 flex h-auto pr-9 px-8 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
           <Link to="/notifs">
             <FontAwesomeIcon className="ml-4" icon={faBell} />
             اعلانات
           </Link>
         </div>
-        <div className="w-auto text-base 2xl:text-xl font-normal items-center py-4 flex h-auto pr-9 px-8 hover:bg-[#F2F2F7] border-none rounded-[75px] text-center">
+        <div className="w-auto text-base 2xl:text-xl font-normal items-center py-4 flex h-auto pr-9 px-8 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
           <Link to="/tags">
             <FontAwesomeIcon className="ml-4" icon={faTag} />
             تگ‌شده‌ها
@@ -91,19 +91,19 @@ export const ProfileSidebar: FunctionComponent = () => {
         </div>
       </div>
       <div className="border-t-2 m-6"></div>
-      <div className="w-auto text-base 2xl:text-xl font-normal items-center py-4 flex h-auto pr-9 px-8 hover:bg-[#F2F2F7] border-none rounded-[75px] text-center">
+      <div className="w-auto text-base 2xl:text-xl font-normal items-center py-4 flex h-auto pr-9 px-8 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
         <Link to="/explore">
           <FontAwesomeIcon className="ml-4" icon={faGripVertical} />
           اکسپلور
         </Link>
       </div>
-      <div className="w-auto text-base 2xl:text-xl font-normal items-center py-4 flex h-auto pr-9 px-8 hover:bg-[#F2F2F7] border-none rounded-[75px] text-center">
+      <div className="w-auto text-base 2xl:text-xl font-normal items-center py-4 flex h-auto pr-9 px-8 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
         <Link to="/search">
           <FontAwesomeIcon className="ml-4" icon={faMagnifyingGlass} />
           جستجو
         </Link>
       </div>
-      <div className="w-auto text-base 2xl:text-xl font-normal mt-[250px] flex h-auto pr-9 px-8 hover:bg-[#F2F2F7] border-none rounded-[75px] text-center">
+      <div className="w-auto text-base 2xl:text-xl font-normal mt-[250px] flex h-auto pr-9 px-8 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
         <Link to="/search">
           <FontAwesomeIcon className="ml-4" icon={faList} />
           بیشتر
