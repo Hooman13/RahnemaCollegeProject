ef411813044bf7e8542c55297b3e650a541d707e
Hooman <hooman.torabi18@gmail.com>
2024-08-28 03:44:53 +0330

    caption page modal changes

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -228,11 +228,9 @@ export const CreatePost: React.FC<IProps> = ({ openModal, setOpenModal }) => {
             {showCaptionPage && (
               <div>
                 {/* The Graph */}
-                <div className="flex-row flex justify-evenly px-4 py-6 ">
+                <div className="flex-row flex justify-evenly px-4 py-4 ">
                   <div className="grid grid-rows-2 justify-items-center">
-                    <div className=" border justify-items-center justify-center items-center  border-[#6F6F6F] rounded-full w-4 h-4">
-                      {/* <div className="flex items-center justify-center border m-auto   border-black rounded-full w-1 h-1 bg-black"></div> */}
-                    </div>
+                    <div className=" border justify-items-center justify-center items-center  border-[#6F6F6F] rounded-full w-4 h-4"></div>
                     <p className="grid text-[#6F6F6F] row-span-1 text-[10px] mt-1">
                       تنظیمات
                     </p>
@@ -244,23 +242,21 @@ export const CreatePost: React.FC<IProps> = ({ openModal, setOpenModal }) => {
                     <p className="grid row-span-1 text-[10px] mt-1">متن</p>
                   </div>
                   <div className="grid grid-rows-2 justify-items-center">
-                    <div className=" border justify-items-center justify-center items-center   border-[#6F6F6F] rounded-full w-4 h-4">
-                      {/* <div className="flex items-center justify-center border m-auto   border-black rounded-full w-1 h-1 bg-black"></div> */}
-                    </div>
+                    <div className=" border justify-items-center justify-center items-center   border-[#6F6F6F] rounded-full w-4 h-4"></div>
                     <p className="grid text-[#6F6F6F] row-span-1 text-[10px] mt-1">
                       عکس
                     </p>
                   </div>
                 </div>
                 {/* main */}
-                <div className="text-center mt-8 text-base font-normal mb-8">
+                <div className="text-center mt-8 text-sm font-normal mb-3">
                   <p>کپشن مورد نظرت رو بنویس:</p>
                 </div>
-                <div className="flex justify-center mb-8">
-                  <div className="text-right text-sm font-bold mb-6 ">
+                <div className="flex justify-center mb-3">
+                  <div className="text-right text-sm font-bold mb-2 ">
                     <p className="text-[#191919] pb-2">کپشن</p>
                     <textarea
-                      className="w-[320px] h-[88px] border solid border-[#17494D]/50 rounded-xl"
+                      className="w-[320px] h-[120px] border solid border-[#17494D]/50 rounded-xl"
                       {...register("caption")}
                       value={formInput.caption}
                       onChange={({ target }) => {
@@ -270,14 +266,15 @@ export const CreatePost: React.FC<IProps> = ({ openModal, setOpenModal }) => {
                   </div>
                 </div>
                 {/* buttons */}
-                <div className="flex items-center justify-end text-sm">
+                <div className="flex items-center justify-end ml-5 text-sm">
                   <div className="flex pl-5">
                     <button onClick={() => setOpenModal(false)}>
                       پشیمون شدم
                     </button>
                   </div>
-                  <div className="text-white text-center mr-1 flex border-solid rounded-2xl bg-[#EA5A69] w-[62px] h-[36px] text-sm justify-center items-center px-[8px] py-[16px] ">
+                  <div>
                     <button
+                      className="text-white text-center mr-1 flex border-solid rounded-2xl bg-[#EA5A69] w-[62px] h-[36px] text-sm justify-center items-center px-[8px] py-[16px] "
                       onClick={() => {
                         setShowCaptionPage(!showCaptionPage);
                         setShowSendPost(!showSendPost);
