1a3d3776e030ac43ec8f68506b9c12ca519bbeac
Hooman <hooman.torabi18@gmail.com>
2024-09-03 23:33:16 +0330

    set post page ui like profile page

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,39 +1,27 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
+import { Header } from "../components/Header";
 import { ProfileSidebar } from "../components/ProfileSidebar";
 import { DisplayPost } from "../components/DisplayPost";
 import { useParams } from "react-router-dom";
 
 export const PostPage = () => {
   const { postId } = useParams();
-    return (
-        <>
-          <div className="w-screen h-full pt-16 px-16 bg-[#F5F5F5]">
-            {/* header */}
-            <div className="grid grid-cols-12 mb-4">
-              <div className="grid col-span-3 py-4 justify-items-center	">
-                <button
-                  type="submit"
-                  // onClick={getProfileData}
-                  className="w-[232px] py-4 px-2 bg-[#EA5A69] rounded-3xl text-white"
-                >
-                  <FontAwesomeIcon className="ml-2" icon={faCirclePlus} />
-                  ایجاد پست جدید
-                </button>
-              </div>
-              <div className=" col-span-9 flex justify-end">
-                <img src="./img/logo.png" alt="" />
-              </div>
-            </div>
-            {/* main */}
-            <div className="grid grid-cols-12">
-              {/* sideBar */}
-              <ProfileSidebar />
-              <div className="mr-16 grid col-span-9">
-                {postId && <DisplayPost postId={postId}/> }
-              </div>
-            </div>
+  return (
+    <>
+      <div className="w-screen  overflow-y-hidden px-7 bg-[#F5F5F5]">
+        {/* header */}
+        <Header />
+        {/* main */}
+        <div className="grid mt-16 overflow-y-hidden grid-cols-12">
+          {/* sideBar */}
+          <div className="col-span-2 pt-2">
+            <ProfileSidebar />
           </div>
-        </>
-      );
+          <div className="mr-60 grid col-span-9">
+            {postId && <DisplayPost postId={postId} />}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
