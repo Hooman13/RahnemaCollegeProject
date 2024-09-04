c333a7ea506b879c27253f710ded1c4b6918212b
Hooman <hooman.torabi18@gmail.com>
2024-09-04 07:15:22 +0330

    set base page

--- src/pages/Notifs.tsx
+++ src/pages/Notifs.tsx
@@ -1,31 +1,86 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
+import { Header } from "../components/Header";
 import { ProfileSidebar } from "../components/ProfileSidebar";
+import { Link, useParams } from "react-router-dom";
+import { FollowingCard } from "../components/FollowingCard";
+import { useState } from "react";
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+import { BaseApi } from "../api/axios";
+import { Button, Modal } from "flowbite-react";
 export const Notifs = () => {
+  interface IUser {
+    username: string;
+    followersCount: number;
+    imageUrl: string;
+  }
+  interface IUsers extends Array<IUser> {}
+  const [followingsData, setFollowingsData] = useState<IUsers>([]);
+  const token = Cookies.get("token");
+  const { username } = useParams();
+  const userName = Cookies.get("username");
+  const userInfoEndpoint = username ? `${username}` : userName;
+  const getFollowingsData = async () => {
+    try {
+      const data: any = await BaseApi.get(userInfoEndpoint + "/followings", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }).then((res) => {
+        const userData = res.data;
+        const followingsList = userData.followings;
+        setFollowingsData((prevState) => ({
+          ...prevState,
+          ...followingsList,
+        }));
+      });
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  useEffect(() => {
+    getFollowingsData();
+  }, []);
   return (
     <>
-      <div className="w-screen h-full pt-16 px-16 bg-[#F5F5F5]">
+      <div className="w-screen  overflow-y-hidden px-7 bg-[#F5F5F5]">
         {/* header */}
-        <div className="grid grid-cols-12 mb-4">
-          <div className="grid col-span-3 py-4 justify-items-center	">
-            <button
-              type="submit"
-              // onClick={getProfileData}
-              className="w-[232px] py-4 px-2 bg-[#EA5A69] rounded-3xl text-white"
-            >
-              <FontAwesomeIcon className="ml-2" icon={faCirclePlus} />
-              ایجاد پست جدید
-            </button>
-          </div>
-          <div className=" col-span-9 flex justify-end">
-            <img src="./img/logo.png" alt="" />
-          </div>
-        </div>
+        <Header />
         {/* main */}
-        <div className="grid grid-cols-12">
+        <div className="grid mt-16 overflow-y-hidden grid-cols-12">
           {/* sideBar */}
-          <ProfileSidebar />
-          <div className="mr-16 grid col-span-9">اعلانات</div>
+          <div className="col-span-2 pt-2">
+            <ProfileSidebar />
+          </div>
+          <div className="mr-10 grid pt-2 col-span-10 overflow-y-scroll	">
+            <h1 className="text-xl font-medium mb-8">اعلانات</h1>
+            <div className="text-md font-normal mb-16 justify-start flex">
+              <Link to="/my-notifs">
+                <button className="text-[#A5A5A5] ml-10">اعلانات من </button>
+              </Link>
+              |
+              <Link to="/friends-notifs">
+                <button className="mr-10">اعلانات دوستان من</button>
+              </Link>
+            </div>
+            <div className="overflow-y-scroll">
+              {Object.values(followingsData).map(function (user, index) {
+                return (
+                  <FollowingCard
+                    username={user.username}
+                    followersCount={user.followersCount}
+                    imageUrl={
+                      user.imageUrl
+                        ? process.env.REACT_APP_IMAGE_URL + user.imageUrl
+                        : "../img/person.png"
+                    }
+                    key={index}
+                  ></FollowingCard>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </>
