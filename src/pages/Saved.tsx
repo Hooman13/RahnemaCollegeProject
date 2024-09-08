59d9b32d7c26666fcba94c431ebf7325aa4782aa
Hooman <hooman.torabi18@gmail.com>
2024-09-08 12:20:19 +0330

    add layout to saved page

--- src/pages/Saved.tsx
+++ src/pages/Saved.tsx
@@ -1,21 +1,12 @@
-import { ProfileSidebar } from "../components/ProfileSidebar";
-import { Header } from "../components/Header";
+import { PagesLayout } from "./PagesLayout";
 
 export const Saved = () => {
   return (
     <>
-      <div className="w-screen  overflow-y-hidden px-7 bg-[#F5F5F5]">
-        {/* header */}
-        <Header />
-        {/* main */}
-        <div className="grid mt-16 overflow-y-hidden grid-cols-12">
-          {/* sideBar */}
-          <div className="col-span-2 pt-2">
-            <ProfileSidebar />
-          </div>
-          <div className="mr-10 grid pt-2 col-span-10 overflow-y-scroll	"></div>
-        </div>
-      </div>
+      <PagesLayout>
+        <p className="text-xl font-semibold mb-3">اکسپلور</p>
+        <div className="w-full bg-inherit flex flex-col justify-center"></div>
+      </PagesLayout>
     </>
   );
 };
