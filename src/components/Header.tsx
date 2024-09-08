f43c9708552de5f32a46ac0e3216f688d00f54c0
Hooman <hooman.torabi18@gmail.com>
2024-09-08 09:09:58 +0330

    set changes to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,23 +7,25 @@ export const Header = () => {
   const [openModal, setOpenModal] = useState(false);
   const [modalSize, setModalSize] = useState<string>("5xl");
   return (
-    <div className="fixed bg-[#F5F5F5] z-50 grid grid-cols-12 w-screen pt-6 mr-2">
-      <div className="grid col-span-2 pb-4 justify-items-center	">
-        <button
-          onClick={() => {
-            setOpenModal(true);
-            setModalSize("xl");
-          }}
-          className="w-auto px-12 text-sm bg-[#EA5A69] rounded-3xl text-white"
-        >
-          <FontAwesomeIcon className="ml-2" icon={faCirclePlus} />
-          پست جدید
-        </button>
-      </div>
-      <div className="col-span-10 mr-[900px] flex items-center">
-        <img className="w-[82px] h-[49px]" src="../img/logo.png" alt="" />
-      </div>
-      <CreatePost openModal={openModal} setOpenModal={setOpenModal} />
-    </div>
+    <header className="relative h-[72px] bg-[#F5F5F5] w-full">
+      <nav className="bg-[#F5F5F5] max-w-7xl mx-auto fixed top-0 z-10 w-full left-1/2 -translate-x-1/2">
+        <div className="flex items-center justify-between py-2 px-4">
+          <button
+            onClick={() => {
+              setOpenModal(true);
+              setModalSize("xl");
+            }}
+            className="w-auto px-12 py-2 text-sm bg-[#EA5A69] rounded-3xl text-white"
+          >
+            <FontAwesomeIcon className="ml-2" icon={faCirclePlus} />
+            پست جدید
+          </button>
+          <div className="col-span-10 flex items-center">
+            <img className="w-[82px] h-[49px]" src="../img/logo.png" alt="" />
+          </div>
+          <CreatePost openModal={openModal} setOpenModal={setOpenModal} />
+        </div>
+      </nav>
+    </header>
   );
 };
