4ffff833cb7272c8d636cbf3fe106e5476a24206
Hooman <hooman.torabi18@gmail.com>
2024-09-08 12:23:37 +0330

    add layout to search page

--- src/pages/Saved.tsx
+++ src/pages/Saved.tsx
@@ -4,7 +4,7 @@ export const Saved = () => {
   return (
     <>
       <PagesLayout>
-        <p className="text-xl font-semibold mb-3">اکسپلور</p>
+        <p className="text-xl font-semibold mb-3">bookmarks</p>
         <div className="w-full bg-inherit flex flex-col justify-center"></div>
       </PagesLayout>
     </>
