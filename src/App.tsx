6822a5befb8409f5f4b782170bfead18081468b9
mostafa hosseini <smostafah89@gmail.com>
2024-09-10 16:26:39 +0330

    feat:install devtools reactquery

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, useState } from "react";
 import { LoginBasePage } from "./pages/LoginBasePage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 export const AuthContext = createContext({});
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <AuthContext.Provider value={{ auth, setAuth }}>
         <div className="App">
           <LoginBasePage />
+          <ReactQueryDevtools initialIsOpen={false}/>
         </div>
       </AuthContext.Provider>
     </QueryClientProvider>
