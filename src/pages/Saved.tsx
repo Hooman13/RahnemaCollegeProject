b4d5f5e5fed9d0af24c84de15c4082e552ebd7a1
Mostafa Hosseini <smostafah89@gmail.com>
2024-09-12 09:23:12 +0000

    Feature/#201/saved posts

--- src/pages/Saved.tsx
+++ src/pages/Saved.tsx
@@ -1,11 +1,14 @@
+import { SavedList } from "../components/SavedList";
 import { PagesLayout } from "./PagesLayout";
 
 export const Saved = () => {
   return (
     <>
       <PagesLayout>
-        <p className="text-xl font-semibold mb-3">bookmarks</p>
-        <div className="w-full bg-inherit flex flex-col justify-center"></div>
+        <p className="text-xl font-semibold mb-3">ذخیره‌ها</p>
+        <div className="w-full bg-inherit flex flex-col justify-center">
+          <SavedList/>
+        </div>
       </PagesLayout>
     </>
   );
