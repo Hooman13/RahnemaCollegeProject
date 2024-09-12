26bd7bfa9ea5806a02a3f7d15971e47d29f1e942
Mostafa Hosseini <smostafah89@gmail.com>
2024-09-12 09:24:57 +0000

    Bugfix/#202/post items must be square

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -19,12 +19,10 @@ export const PostItem: FunctionComponent<PropsWithChildren<IPostProps>> = ({
         className="relative max-w-sm cursor-pointer hover:shadow-lg"
       >
         <img
-          className="h-[304px] w-[304px] rounded-3xl aspect-square object-cover transition-transform duration-300 transform hover:scale-105 peer"
+          className="rounded-3xl aspect-square object-cover transition-transform duration-300 transform hover:scale-105 peer"
           src={imgUrl}
           alt=""
         />
-
-        <figcaption className="absolute px-4 text-sm text-white bottom-6 w-100"></figcaption>
       </figure>
       {openModal && (
         <PostModal
