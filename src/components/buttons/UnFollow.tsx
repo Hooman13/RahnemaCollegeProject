ce1522b1d8bcb65f7271e507f17b4d1fa47686ef
Hooman <hooman.torabi18@gmail.com>
2024-09-20 15:11:23 +0330

    modify unfollow button padding

--- src/components/buttons/UnFollow.tsx
+++ src/components/buttons/UnFollow.tsx
@@ -74,7 +74,7 @@ export const UnFollow: React.FC<PropsWithChildren<IUser>> = ({
         <button
           onClick={handleUnFollow}
           type="button"
-          className="text-xs font-semibold py-1 px-6 rounded-[100px] border border-[#EA5A69] text-[#EA5A69]"
+          className="text-xs font-semibold py-1 px-5 rounded-[100px] border border-[#EA5A69] text-[#EA5A69]"
         >
           دنبال نکردن
         </button>
