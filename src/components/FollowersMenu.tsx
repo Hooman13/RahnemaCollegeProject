2d6df813f72b9b57ca45d722886a943cd72eb24f
Hooman <hooman.torabi18@gmail.com>
2024-09-21 15:01:55 +0330

    add button to followers following menu

--- src/components/FollowersMenu.tsx
+++ src/components/FollowersMenu.tsx
@@ -4,43 +4,62 @@ import { faPlus, faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 import React, { useState, PropsWithChildren } from "react";
 import { RemoveFollowing } from "./buttons/RemoveFollowing";
 import { RemoveFollower } from "./buttons/RemoveFollower";
+import { ChatModal } from "../features/chat/components/ChatModal";
 
 interface IUser {
   user: string;
+  relation?: string;
+  fullname?: string;
+  imgUrl?: string;
 }
 
 export const FollowerMenu: React.FC<PropsWithChildren<IUser>> = ({
   user,
+  relation,
   children,
+  fullname,
+  imgUrl,
 }) => {
+  const [openModal, setOpenModal] = useState(false);
   return (
-    <Menu as="div" className="relative inline-block">
-      <div>
-        <MenuButton className="text-[#EA5A69] text-4xl items-end">
-          <FontAwesomeIcon icon={faEllipsisVertical} />
-        </MenuButton>
-      </div>
-
-      <MenuItems
-        transition
-        className="absolute left-0 z-10 w-56 origin-top-right rounded-tr-lg rounded-b-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-      >
-        <div className="py-3">
-          <MenuItem>
-            <a
-              href="#"
-              className="block px-4 py-2 text-xs font-normal text-Black data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-            >
-              پیام
-            </a>
-          </MenuItem>
-          <MenuItem>
-            <div className=" data-[focus]:bg-gray-100">
-              <RemoveFollower user={user} />
-            </div>
-          </MenuItem>
+    <>
+      {openModal && (
+        <ChatModal
+          fullname={fullname}
+          imgUrl={imgUrl}
+          openModal={openModal}
+          setOpenModal={setOpenModal}
+          reciverUsername={user}
+        />
+      )}
+      <Menu as="div" className="relative inline-block">
+        <div>
+          <MenuButton className="text-[#EA5A69] text-4xl items-end">
+            <FontAwesomeIcon icon={faEllipsisVertical} />
+          </MenuButton>
         </div>
-      </MenuItems>
-    </Menu>
+
+        <MenuItems
+          transition
+          className="absolute left-0 z-10 w-56 origin-top-right rounded-tr-lg rounded-b-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+        >
+          <div className="py-3">
+            <MenuItem>
+              <button
+                onClick={() => setOpenModal(true)}
+                className="block px-4 py-2 text-xs font-normal text-Black data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+              >
+                پیام
+              </button>
+            </MenuItem>
+            <MenuItem>
+              <div className=" data-[focus]:bg-gray-100">
+                <RemoveFollower user={user} />
+              </div>
+            </MenuItem>
+          </div>
+        </MenuItems>
+      </Menu>
+    </>
   );
 };
