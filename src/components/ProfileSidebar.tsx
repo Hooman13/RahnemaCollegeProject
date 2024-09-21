63f9b012f8405abd9479d005fc13af8b474761e9
Hooman <hooman.torabi18@gmail.com>
2024-09-22 01:58:38 +0330

    text sizes and padings correction

--- src/components/ProfileSidebar.tsx
+++ src/components/ProfileSidebar.tsx
@@ -45,9 +45,9 @@ export const ProfileSidebar: FunctionComponent = () => {
     <div className="text-sm rounded-t-[30px] w-full h-full relative">
       <div className="border border-[#A5A5A5] w-full sticky top-24 bg-white rounded-t-2xl min-h-[90vh]">
         <div>
-          <div className="items-center flex h-auto mt-3 mr-6 py-2 ">
+          <div className="items-center flex h-auto mt-3 pr-2 lg:pr-6 py-2 ">
             <img
-              className="border rounded-full w-10 2xl:w-14 h-10 2xl:h-14 mt-3"
+              className="border rounded-full w-10 2xl:w-14 h-10 2xl:h-14"
               src={
                 data?.imageUrl
                   ? process.env.REACT_APP_IMAGE_URL + data?.imageUrl
@@ -58,41 +58,41 @@ export const ProfileSidebar: FunctionComponent = () => {
             <span className="px-4">{data?.username}</span>
           </div>
           <Link to="/profile">
-            <div className="w-auto  2xl:text-xl font-normal items-center py-3 flex h-auto pr-9 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
+            <div className="w-auto font-normal items-center py-3 flex h-auto pr-3 lg:pr-9 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
               <FontAwesomeIcon className="ml-4" icon={faThumbTack} />
               صفحه من
             </div>
           </Link>
           <Link to="/saved">
-            <div className="w-auto 2xl:text-xl font-normal items-center py-3 flex h-auto pr-9 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
+            <div className="w-auto font-normal items-center py-3 flex h-auto pr-3 lg:pr-9 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
               <FontAwesomeIcon className="ml-4" icon={faBookmark} />
               ذخیره‌ها
             </div>
           </Link>
           <Link to="/messages">
-            <div className="w-auto 2xl:text-xl font-normal items-center py-3 flex h-auto pr-9 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
+            <div className="w-auto font-normal items-center py-3 flex h-auto pr-3 lg:pr-9 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
               <FontAwesomeIcon className="ml-4" icon={faCommentDots} />
               پیام‌ها
               {data?.messagesCount && data?.messagesCount !== 0 ? (
-                <div className="flex justify-center justify-items-center mr-14 w-6 h-6 border border-[#F6881F] rounded-full bg-[#F6881F] text-base">
+                <div className="flex justify-center justify-items-center  mr-6 md:mr-10  lg:mr-14 w-6 h-6 border border-[#F6881F] rounded-full bg-[#F6881F] text-base">
                   <p>{data?.messagesCount}</p>
                 </div>
               ) : null}
             </div>
           </Link>
           <Link to="/notifs">
-            <div className="w-auto 2xl:text-xl font-normal items-center py-3 flex h-auto pr-9 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
+            <div className="w-auto font-normal items-center py-3 flex h-auto pr-3 lg:pr-9 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
               <FontAwesomeIcon className="ml-4" icon={faBell} />
               اعلانات
               {data?.notifsCount && data?.notifsCount !== 0 ? (
-                <div className="flex justify-center justify-items-center mr-14 w-6 h-6 border border-[#F6881F] rounded-full bg-[#F6881F] text-base">
+                <div className="flex justify-center justify-items-center mr-6 md:mr-10 lg:mr-14 w-6 h-6 border border-[#F6881F] rounded-full bg-[#F6881F] text-base">
                   <p>{data?.notifsCount}</p>
                 </div>
               ) : null}
             </div>
           </Link>
           <Link to="/tags">
-            <div className="w-auto 2xl:text-xl font-normal items-center py-3 flex h-auto pr-9 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
+            <div className="w-auto font-normal items-center py-3 flex h-auto pr-3 lg:pr-9 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
               <FontAwesomeIcon className="ml-4" icon={faTag} />
               تگ‌شده‌ها
             </div>
@@ -100,18 +100,18 @@ export const ProfileSidebar: FunctionComponent = () => {
         </div>
         <div className="border-t-2 m-2"></div>
         <Link to="/explore">
-          <div className="w-auto 2xl:text-xl font-normal items-center py-3 flex h-auto pr-9  hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
+          <div className="w-auto font-normal items-center py-3 flex h-auto pr-3 lg:pr-9 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
             <FontAwesomeIcon className="ml-4" icon={faGripVertical} />
             اکسپلور
           </div>
         </Link>
         <Link to="/search-people">
-          <div className="w-auto 2xl:text-xl font-normal items-center py-3 flex h-auto pr-9 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
+          <div className="w-auto font-normal items-center py-3 flex h-auto pr-3 lg:pr-9 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
             <FontAwesomeIcon className="ml-4" icon={faMagnifyingGlass} />
             جستجو
           </div>
         </Link>
-        <div className="w-auto 2xl:text-xl font-normal items-center py-3 flex h-auto pr-9 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
+        <div className="w-auto font-normal items-center py-3 flex h-auto pr-3 lg:pr-9 hover:bg-[#F5F5F5] border-none rounded-[75px] text-center">
           <button
             onClick={() => {
               logout();
