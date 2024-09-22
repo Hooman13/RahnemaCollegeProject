8c3751dad03ab1b918ced7c0a7dc2a331bbf3d60
Hooman <hooman.torabi18@gmail.com>
2024-09-22 03:43:42 +0330

    set cookies

--- src/components/PostBookmark.tsx
+++ src/components/PostBookmark.tsx
@@ -20,7 +20,20 @@ export const PostBookmark: React.FC<IProps> = ({
   type,
 }) => {
   const [isLoading, setIsLoading] = useState(false);
-  const token = Cookies.get("token");
+  const selectedAccount = Cookies.get("selectedAccount");
+  const currentTokenCookie = Cookies.get("token");
+  const token =
+    currentTokenCookie && selectedAccount
+      ? JSON.parse(currentTokenCookie)[parseInt(selectedAccount)]
+      : null;
+
+  const currentUsernameCookie = Cookies.get("username");
+  const cookieUsername =
+    currentUsernameCookie && selectedAccount
+      ? JSON.parse(currentUsernameCookie)[parseInt(selectedAccount)]
+      : null;
+  // const profileUsername = username ? `${username}` : cookieUsername;
+  const profileUsername = cookieUsername;
   interface IBookmark {
     bookMarkCount: number;
     isBookMarked: boolean;
